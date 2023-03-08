98ff925567a615ba0337a34f405a2222e98ab00b
Redy <redy.ru@gmail.com>
2023-03-09 02:08:51 +0800

    start

---
+++ media/lua/server/Items/ChestRigDistributions.lua
@@ -0,0 +1,86 @@
+require "Items/Distributions"
+require "Items/ProceduralDistributions"
+require "Vehicles/VehicleDistributions"
+
+if ProceduralDistributions.list.DrugLabGuns then 
+	table.insert(ProceduralDistributions.list.DrugLabGuns.items, "Webbing")
+	table.insert(ProceduralDistributions.list.DrugLabGuns.items, 1)	
+	table.insert(ProceduralDistributions.list.DrugLabGuns.items, "ChestRig")
+	table.insert(ProceduralDistributions.list.DrugLabGuns.items, 1)
+end
+
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, "Webbing")
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, 1)	
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, "ChestRig")
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, 1)
+
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "Webbing_Military")
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 0.5)	
+
+table.insert(ProceduralDistributions.list.FirearmWeapons.items, "ChestRig")
+table.insert(ProceduralDistributions.list.FirearmWeapons.items, 1)	
+table.insert(ProceduralDistributions.list.FirearmWeapons.items, "Webbing")
+table.insert(ProceduralDistributions.list.FirearmWeapons.items, 1)
+
+table.insert(ProceduralDistributions.list.GarageFirearms.items, "ChestRig")
+table.insert(ProceduralDistributions.list.GarageFirearms.items, 0.1)	
+table.insert(ProceduralDistributions.list.GarageFirearms.items, "Webbing")
+table.insert(ProceduralDistributions.list.GarageFirearms.items, 0.1)
+
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Webbing")
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 0.1)	
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "ChestRig")
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 0.5)
+
+table.insert(ProceduralDistributions.list.LockerArmyBedroom.items, "Webbing_Military")
+table.insert(ProceduralDistributions.list.LockerArmyBedroom.items, 0.5)
+
+table.insert(ProceduralDistributions.list.PawnShopGunsSpecial.items, "ChestRig")
+table.insert(ProceduralDistributions.list.PawnShopGunsSpecial.items, 1)
+table.insert(ProceduralDistributions.list.PawnShopGunsSpecial.items, "Webbing")
+table.insert(ProceduralDistributions.list.PawnShopGunsSpecial.items, 1)
+
+table.insert(ProceduralDistributions.list.PlankStashGun.items, "ChestRig")
+table.insert(ProceduralDistributions.list.PlankStashGun.items, 1)
+table.insert(ProceduralDistributions.list.PlankStashGun.items, "Webbing")
+table.insert(ProceduralDistributions.list.PlankStashGun.items, 1)
+
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, "Webbing_Black")
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, 0.5)
+
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, "ChestRig")
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, 0.1)
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, "Webbing")
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, 0.1)
+
+
+table.insert(SuburbsDistributions.Bag_WeaponBag.items, "ChestRig")
+table.insert(SuburbsDistributions.Bag_WeaponBag.items, 0.1)
+table.insert(SuburbsDistributions.Bag_WeaponBag.items, "Webbing")
+table.insert(SuburbsDistributions.Bag_WeaponBag.items, 0.1)
+
+table.insert(SuburbsDistributions.Bag_SurvivorBag.items, "ChestRig")
+table.insert(SuburbsDistributions.Bag_SurvivorBag.items, 0.1)
+table.insert(SuburbsDistributions.Bag_SurvivorBag.items, "Webbing")
+table.insert(SuburbsDistributions.Bag_SurvivorBag.items, 0.1)
+
+table.insert(SuburbsDistributions.SurvivorCache1.SurvivorCrate.items, "ChestRig")
+table.insert(SuburbsDistributions.SurvivorCache1.SurvivorCrate.items, 0.1)
+table.insert(SuburbsDistributions.SurvivorCache1.SurvivorCrate.items, "Webbing")
+table.insert(SuburbsDistributions.SurvivorCache1.SurvivorCrate.items, 0.5)
+
+table.insert(SuburbsDistributions.SurvivorCache2.SurvivorCrate.items, "ChestRig")
+table.insert(SuburbsDistributions.SurvivorCache2.SurvivorCrate.items, 0.1)
+table.insert(SuburbsDistributions.SurvivorCache2.SurvivorCrate.items, "Webbing")
+table.insert(SuburbsDistributions.SurvivorCache2.SurvivorCrate.items, 0.5)	
+
+
+table.insert(VehicleDistributions.Police.TruckBed.items, "Webbing_Military")
+table.insert(VehicleDistributions.Police.TruckBed.items, 0.5)
+
+table.insert(VehicleDistributions.SurvivalistTruckBed.items, "ChestRig")
+table.insert(VehicleDistributions.SurvivalistTruckBed.items, 0.5)		
+table.insert(VehicleDistributions.SurvivalistTruckBed.items, "Webbing")
+table.insert(VehicleDistributions.SurvivalistTruckBed.items, 0.5)
+	
+	
